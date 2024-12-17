fc13b185fb90259b9800fb601e72501a40c46fcc
Jiang <Zhibin.jiang2023@gmail.com>
2024-12-17 18:26:11 +0800

    delete files

--- README.md
+++
@@ -1,6 +0,0 @@
-# Java-Hand-Writing-Digit-Recognition
-
-![image-20241217173028697](./imageResource/image-20241217173028697.png)
-
-## 界面介绍
-
--- src/Application.java
+++
@@ -1,9 +0,0 @@
-import functions.GUI;
-import java.io.IOException;
-
-public class Application {
-    public static void main(String[] args) throws IOException {
-        new GUI();
-    }
-
-}
